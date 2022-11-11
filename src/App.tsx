97cc627f9eefedcf151925df0902ed319fc5bad1
Abner Lima <67610973+abnerlimaab@users.noreply.github.com>
2022-11-10 21:35:24 -0300

    Revert "Feature/2"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-import { useMemo } from "react";
-import { createCalendarObject } from "./functions";
-import { ICalendar } from "./interfaces";
-
 function App() {
-  const years: ICalendar = useMemo(() => createCalendarObject(2022), []);
-
-  console.log(years.years[0]);
   return <></>;
 }
 
